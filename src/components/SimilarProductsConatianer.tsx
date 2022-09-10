377149115271b3260f0b6d3fa24e1c1941565912
Arpan Dhama <arpandhama2001@gmail.com>
2022-09-10 17:13:51 +0530

    empty similar products section showing fixed

--- src/components/SimilarProductsConatianer.tsx
+++ src/components/SimilarProductsConatianer.tsx
@@ -24,7 +24,11 @@ const SimilarProductsConatianer = () => {
   }
 
   return (
-    <section className=" overflow-auto w-[80%] bg-white rounded-lg mt-4 p-6 mx-auto divide-y divide-neutral-200">
+    <section
+      className={`${
+        similarP.length ? "" : "hidden"
+      } overflow-auto w-[80%] bg-white rounded-lg mt-4 p-6 mx-auto divide-y divide-neutral-200`}
+    >
       <h2 className="p-4 font-secondary text-xl text-neutral-700 ">
         You might be interested in:
       </h2>
